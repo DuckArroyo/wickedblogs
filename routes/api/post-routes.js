4e9825f391fd561d10f0386ef94b7cdb7b87db7d
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-10 01:14:48 -0800

    completed 13.3, blog posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 //We connect to post and user to have the full set of data.
 const { Post, User } = require("../../models");
 
-// get all users
+// get all users - works DESC
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
     //Find the posts with these columns
     attributes: ["id", "blog_post", "title", "created_at"],
+    //In this order
+    order: [["created_at", "DESC"]],
     //also give me the User name
     include: [
       {
@@ -17,6 +19,119 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Get all users ASC
+router.get("/ASC", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    //Find the posts with these columns
+    attributes: ["id", "blog_post", "title", "created_at"],
+    //In this order
+    order: [["created_at", "ASC"]],
+    //also give me the User name
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Get this one post //! I think this will be important for the blog
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "blog_post", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Create a post - works
+router.post("/", (req, res) => {
+  // expects {title: 'Taskmaster goes public!', blog_post: 'blah blah', user_id: 1}
+  //Equivalent of Insert in SQL
+  Post.create({
+    title: req.body.title,
+    blog_post: req.body.blog_post,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//UPDATE a post  - works
+router.put("/:id", (req, res) => {
+  // expects {title: 'Taskmaster goes public!', blog_post: 'blah blah', user_id: #}
+  Post.update(
+    {
+      title: req.body.title,
+      blog_post: req.body.blog_post,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//DELETE a post - Works
+router.delete("/:id", (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
